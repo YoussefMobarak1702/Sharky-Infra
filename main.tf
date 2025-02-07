7a1ada0edd29a0da7697e4f9cb861fe076c0791a
YoussefMobarak1702 <ym17021995@gmail.com>
2025-02-07 19:54:50 +0200

    fix: update infra

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "gcs" {
+    bucket = "tfstatesharky"
+  }
+}
+data "google_container_engine_versions" "gke_version" {
+  location       = var.region
+  version_prefix = "1.27."
+}
+
+resource "google_container_cluster" "sharky-primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "sharky-primary_nodes" {
+  name     = google_container_cluster.sharky-primary.name
+  location = var.region
+  cluster  = google_container_cluster.sharky-primary.name
+
+  version    = "1.30.8-gke.1162001"
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
+variable "project_id" {
+
+  default     = "sharky-enterprises-450214"
+  description = "project id"
+}
+
+variable "region" {
+  default     = "europe-west8"
+  description = "region"
+}
